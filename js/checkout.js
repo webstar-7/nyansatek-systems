6023a3090f963148f014b0ed99a93838cb528ab1
webstar-7 <expitoderoy@gmail.com>
2026-09-02 20:42:19 +0000

    Update Paystack public key to live version

--- js/checkout.js
+++ js/checkout.js
@@ -10,7 +10,7 @@
    window.PAYSTACK_PUBLIC_KEY set below to your real key.
    ============================================================ */
 
-const PAYSTACK_PUBLIC_KEY = "pk_test_REPLACE_WITH_YOUR_PAYSTACK_PUBLIC_KEY";
+const PAYSTACK_PUBLIC_KEY = "pk_live_f06348c9521967084c78419c709c740c7d3b51d2";
 
 const params = new URLSearchParams(window.location.search);
 const productId = params.get("product") || "pos";
